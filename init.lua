7b25790af833b7ac67eddf85efe8bb4fddf2854c
tomasquintana-gojiraf <tomas.quintana@gojiraf.com>
2022-12-03 12:20:00 -0300

    General fyle

---
+++ init.lua
@@ -0,0 +1,33 @@
+require('keymaps')
+require('theme')
+require('plugins')
+require('settings')
+require('statusBar')
+require('tree')
+require('bufferLine')
+require('tmux')
+--require('lspColors')
+--require ('commentsCode')
+require('gitSing')
+--require('autopairs')
+require('treesiter')
+require('lspConfig')
+--NOTE: arreglar telescope find files
+--require ('_telescope')  
+require('fugitive') -- me permite usar git dentro de nvim
+require("neofugitive") -- similar a vim fugitive
+--require("autoTag")
+require("configTodoComments")
+--require("reactAutopair")
+require('nvim-cmp')
+--require('preview-Markdown')
+--require('markdownPre')
+--require('test')
+--require('test-eslint')
+--require('eslint')
+--require('mason')
+
+--TODO: 
+
+
+--TODO: commitear y pushear los camvios en estatus line, lsp config y ver atajo para diagnostico
